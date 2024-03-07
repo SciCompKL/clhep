248345806edb337383f98b7e0323668b724f1e29
Max Aehle <max.aehle@scicomp.uni-kl.de>
2024-03-07 11:37:17 +0100

    Make CLHEPfloat a different type than CLHEPdouble

--- CLHEPTypes.hpp
+++ CLHEPTypes.hpp
@@ -1,3 +1,10 @@
+#pragma once
 #include "easyAD.hpp"
 using CLHEPdouble  = Forward;
-using CLHEPfloat = Forward;
+struct CLHEPfloat : public Forward {
+  template<typename...Args>
+  CLHEPfloat(Args&&...args): Forward(std::forward<Args>(args)...) {}
+  /*CLHEPfloat(Forward f): Forward(f) {}
+  CLHEPfloat() = default;
+  CLHEPfloat(double val): Forward(val) {}*/
+};
